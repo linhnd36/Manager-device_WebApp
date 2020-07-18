a4e63b8192e564177d2ad80f788bdb320ab08c3d
= <=>
2020-07-18 23:30:35 +0700

    filter done

---
+++ ManagerResource/src/java/linhnd/filters/MainFilter.java
@@ -0,0 +1,278 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linhnd.filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import linhnd.dtos.UsersDTO;
+
+/**
+ *
+ * @author Duc Linh
+ */
+public class MainFilter implements Filter {
+
+    private static final boolean debug = true;
+
+    // The filter configuration object we are associated with.  If
+    // this value is null, this filter instance is not currently
+    // configured. 
+    private FilterConfig filterConfig = null;
+
+    private final List<String> guests;
+    private final List<String> manager;
+    private final List<String> leaderEmp;
+
+    public MainFilter() {
+        guests = new ArrayList<>();
+        guests.add("login.jsp");
+        guests.add("confirmMail.jsp");
+        guests.add("error.jsp");
+        guests.add("signup.jsp");
+        guests.add("");
+        guests.add("''");
+        guests.add("login");
+        guests.add("login.action");
+        guests.add("logout");
+        guests.add("logout.action");
+        guests.add("signup");
+        guests.add("signup.action");
+        guests.add("send-code-mail");
+        guests.add("send-code-mail.action");
+        guests.add("confirm-mail");
+        guests.add("confirm-mail.action");
+
+        manager = new ArrayList<>();
+        manager.add("login.jsp");
+        manager.add("manager.jsp");
+        manager.add("manager");
+        manager.add("manager.action");
+
+        leaderEmp = new ArrayList<>();
+        leaderEmp.add("login.jsp");
+        leaderEmp.add("login");
+        leaderEmp.add("login.action");
+        leaderEmp.add("search.jsp");
+        leaderEmp.add("viewBooking.jsp");
+        leaderEmp.add("search-action");
+        leaderEmp.add("search-action.action");
+        leaderEmp.add("add-to-card");
+        leaderEmp.add("add-to-card.action");
+        leaderEmp.add("view-booking");
+        leaderEmp.add("view-booking.action");
+
+    }
+
+    private void doBeforeProcessing(ServletRequest request, ServletResponse response)
+            throws IOException, ServletException {
+        if (debug) {
+            log("MainFilter:DoBeforeProcessing");
+        }
+
+        // Write code here to process the request and/or response before
+        // the rest of the filter chain is invoked.
+        // For example, a logging filter might log items on the request object,
+        // such as the parameters.
+        /*
+	for (Enumeration en = request.getParameterNames(); en.hasMoreElements(); ) {
+	    String name = (String)en.nextElement();
+	    String values[] = request.getParameterValues(name);
+	    int n = values.length;
+	    StringBuffer buf = new StringBuffer();
+	    buf.append(name);
+	    buf.append("=");
+	    for(int i=0; i < n; i++) {
+	        buf.append(values[i]);
+	        if (i < n-1)
+	            buf.append(",");
+	    }
+	    log(buf.toString());
+	}
+         */
+    }
+
+    private void doAfterProcessing(ServletRequest request, ServletResponse response)
+            throws IOException, ServletException {
+        if (debug) {
+            log("MainFilter:DoAfterProcessing");
+        }
+
+        // Write code here to process the request and/or response after
+        // the rest of the filter chain is invoked.
+        // For example, a logging filter might log the attributes on the
+        // request object after the request has been processed. 
+        /*
+	for (Enumeration en = request.getAttributeNames(); en.hasMoreElements(); ) {
+	    String name = (String)en.nextElement();
+	    Object value = request.getAttribute(name);
+	    log("attribute: " + name + "=" + value.toString());
+
+	}
+         */
+        // For example, a filter might append something to the response.
+        /*
+	PrintWriter respOut = new PrintWriter(response.getWriter());
+	respOut.println("<P><B>This has been appended by an intrusive filter.</B>");
+         */
+    }
+
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        String uri = req.getRequestURI();
+        int index = uri.lastIndexOf("/");
+        String resource = uri.substring(index + 1);
+
+        HttpSession session = req.getSession(false);
+        if (resource.contains(".png")
+                || resource.contains(".css")
+                || resource.contains(".jpg")) {
+            chain.doFilter(request, response);
+        } else {
+            if (session == null || session.getAttribute("USER") == null) {
+                if (guests.contains(resource)) {
+                    chain.doFilter(request, response);
+                } else {
+                }
+            } else {
+                UsersDTO dto = (UsersDTO) session.getAttribute("USER");
+                if (dto.getRoleId().contains("LEADER") && leaderEmp.contains(resource)) {
+                    chain.doFilter(request, response);
+                } else if (dto.getRoleId().contains("MANAGER") && manager.contains(resource)) {
+                    chain.doFilter(request, response);
+                } else if (dto.getRoleId().contains("EMP") && leaderEmp.contains(resource)) {
+                    chain.doFilter(request, response);
+                } else {
+                    res.sendRedirect("login.jsp");
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Return the filter configuration object for this filter.
+     */
+    public FilterConfig getFilterConfig() {
+        return (this.filterConfig);
+    }
+
+    /**
+     * Set the filter configuration object for this filter.
+     *
+     * @param filterConfig The filter configuration object
+     */
+    public void setFilterConfig(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+    }
+
+    /**
+     * Destroy method for this filter
+     */
+    public void destroy() {
+    }
+
+    /**
+     * Init method for this filter
+     */
+    public void init(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+        if (filterConfig != null) {
+            if (debug) {
+                log("MainFilter:Initializing filter");
+            }
+        }
+    }
+
+    /**
+     * Return a String representation of this object.
+     */
+    @Override
+    public String toString() {
+        if (filterConfig == null) {
+            return ("MainFilter()");
+        }
+        StringBuffer sb = new StringBuffer("MainFilter(");
+        sb.append(filterConfig);
+        sb.append(")");
+        return (sb.toString());
+    }
+
+    private void sendProcessingError(Throwable t, ServletResponse response) {
+        String stackTrace = getStackTrace(t);
+
+        if (stackTrace != null && !stackTrace.equals("")) {
+            try {
+                response.setContentType("text/html");
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                PrintWriter pw = new PrintWriter(ps);
+                pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); //NOI18N
+
+                // PENDING! Localize this for next official release
+                pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");
+                pw.print(stackTrace);
+                pw.print("</pre></body>\n</html>"); //NOI18N
+                pw.close();
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        } else {
+            try {
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                t.printStackTrace(ps);
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+    public static String getStackTrace(Throwable t) {
+        String stackTrace = null;
+        try {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            t.printStackTrace(pw);
+            pw.close();
+            sw.close();
+            stackTrace = sw.getBuffer().toString();
+        } catch (Exception ex) {
+        }
+        return stackTrace;
+    }
+
+    public void log(String msg) {
+        filterConfig.getServletContext().log(msg);
+    }
+
+}
